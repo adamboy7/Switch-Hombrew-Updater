1988a09e71963c43ccdb340bfd2b26bb86a5b734
adamboy7 <adamboy8888@gmail.com>
2026-02-20 09:26:07 -0500

    Clear nested directories when emptying Downloads

--- actions/clear_downloads.py
+++ actions/clear_downloads.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import tkinter.messagebox as messagebox
 
 from actions.config import DOWNLOADS_DIR_NAME
@@ -7,10 +8,12 @@ def clear_downloads_action():
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
     output_dir = os.path.join(base_dir, DOWNLOADS_DIR_NAME)
     if os.path.exists(output_dir):
-        for file in os.listdir(output_dir):
-            file_path = os.path.join(output_dir, file)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        messagebox.showinfo("Clear Downloads", "All downloads have been cleared.")
+        for entry in os.listdir(output_dir):
+            entry_path = os.path.join(output_dir, entry)
+            if os.path.isfile(entry_path):
+                os.remove(entry_path)
+            elif os.path.isdir(entry_path):
+                shutil.rmtree(entry_path)
+        messagebox.showinfo("Clear Downloads", "All contents in Downloads have been fully cleared.")
     else:
         messagebox.showinfo("Clear Downloads", "No downloads to clear.")
